ac9103493242f14beea97aaec7a9a45f8ead4f6f
Rafael Kähm <rafael@kaehms.de>
2015-09-03 16:35:27 +0200

    cron script for proper shutdown fixed

--- cubietruck/UPS_LOW_BATTERY_SHUTDOWN.sh
+++ cubietruck/UPS_LOW_BATTERY_SHUTDOWN.sh
@@ -6,27 +6,28 @@
 DATE=`date +"%Y.%m.%d %T"`
 
 BATTERY_CONNECTED=`power_supply BATTERY_CONNECTED`
-if [ $BATTERY_CONNECTED != 1 ] ; then
+if [[ $BATTERY_CONNECTED != 1 ]] ; then
 	echo "Battery not connected!"
-    exit
+	exit 0;
 fi
-
 
 BATTERY_ONLINE=`power_supply BATTERY_ONLINE`
 if [[ $BATTERY_ONLINE -eq 0 ]] ; then
 	echo "Battery not consumed!"
-    exit
+	exit 0;
 fi
 
-if [[ $BATTERY_VOLTAGE -le 3600 ]]
-then
+BATTERY_VOLTAGE=`power_supply BATTERY_VOLTAGE`
+if [[ $BATTERY_VOLTAGE -le 3600 ]] ; then
 	BATTERY_VOLTAGE_DECIMAL=$(echo "scale=2; ${BATTERY_VOLTAGE}/1000"|bc -l)V
-    echo -e "######################################################
-#   $DATE: Low battery capacity: $BATTERY_LEVEL%   #
-#   Low battery voltage: $BATTERY_VOLTAGE_DECIMAL                       #
-#   System shuts down now!                           #
-######################################################
-" >> /var/log/ups.log
-    shutdown -h now
-    exit 0;
+	BATTERY_LEVEL=`power_supply BATTERY_LEVEL`%
+
+	echo -e "######################################################" >> /var/log/ups.log
+	echo -e "  $DATE: Low battery capacity: $BATTERY_LEVEL" >> /var/log/ups.log
+	echo -e "    Low battery voltage: $BATTERY_VOLTAGE_DECIMAL" >> /var/log/ups.log
+	echo -e "    System shuts down now!" >> /var/log/ups.log
+	echo -e "######################################################" >> /var/log/ups.log
+
+	shutdown -h now
+	exit 0;
 fi
