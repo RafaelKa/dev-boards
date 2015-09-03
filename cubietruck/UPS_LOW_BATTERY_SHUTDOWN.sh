b1a9f1ffd33bae904056d4f3a312c734b7982926
Rafael Kähm <rafael@kaehms.de>
2015-09-03 15:43:30 +0200

    Cron script for proper shutdown by low battery added

---
+++ cubietruck/UPS_LOW_BATTERY_SHUTDOWN.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# This script is for cron
+#
+
+
+DATE=`date +"%Y.%m.%d %T"`
+
+BATTERY_CONNECTED=`./power_supply BATTERY_CONNECTED`
+if [ $BATTERY_CONNECTED != 1 ] ; then
+	echo "Battery not connected!"
+    exit
+fi
+
+
+BATTERY_ONLINE=`./power_supply BATTERY_ONLINE`
+if [[ $BATTERY_ONLINE -eq 0 ]] ; then
+	echo "Battery not consumed!"
+    exit
+fi
+
+BATTERY_VOLTAGE=`./power_supply BATTERY_VOLTAGE`
+BATTERY_LEVEL=`./power_supply BATTERY_LEVEL`
+if [[ $BATTERY_VOLTAGE -le 4100 ]]
+then
+	BATTERY_VOLTAGE_DECIMAL=$(echo "scale=2; ${BATTERY_VOLTAGE}/1000"|bc -l)V
+    echo -e "######################################################
+#   $DATE: Low battery capacity: $BATTERY_LEVEL%   #
+#   Low battery voltage: $BATTERY_VOLTAGE_DECIMAL                       #
+#   System shuts down now!                           #
+######################################################
+" >> /var/log/ups.log
+    shutdown -h now
+    exit 0;
+fi
